32de38ed5e9040114102f00325fb0975224f54f2
FDlucifer <lucifercaonima@gmail.com>
2022-11-13 06:17:16 +0800

    lucifer11

--- leetcode-python-go/meituan-010. 小团的默契游戏/main.py
+++ leetcode-python-go/meituan-010. 小团的默契游戏/main.py
@@ -4,8 +4,10 @@ nums = list(map(int, input().split()))
 def judge(l, r):
     _min = 0
     for x in nums:
-        if l <= x <= r: continue
-        if x < _min: return False
+        if l <= x <= r:
+            continue
+        if x < _min:
+            return False
         _min = x
     return True
 
@@ -13,14 +15,17 @@ def getR(left):
     l, r = left, length
     while l <= r:
         mid = (l + r) >> 1
-        if judge(left, mid): r = mid - 1
-        else: l = mid + 1
+        if judge(left, mid):
+            r = mid - 1
+        else:
+            l = mid + 1
     return l
 
 res = 0
 for left in range(1, m + 1):
     right = getR(left)
-    if right == length + 1: break
+    if right == length + 1:
+        break
     res += length - right + 1
 
 print(res)
