ce3b231284a4f75a08fc2c9101c95e0d57b6535d
FDlucifer <lucifercaonima@gmail.com>
2023-05-13 03:07:52 +0800

    luci11

--- README.md
+++ README.md
@@ -173,6 +173,7 @@ from [0xdf](https://www.youtube.com/c/0xdf0xdf)'s AOC 2015 code from DAY 0 - DAY
  - Day 5: Supply Stacks
  - Day 6: Tuning Trouble
  - Day 7: No Space Left On Device
+ - Day 8: Treetop Tree House
  -
 
 ## Notes From Leetcode with Python && Golang
--- golang/AOC2022/DAY7/main.py
+++ golang/AOC2022/DAY7/main.py
@@ -1,8 +1,37 @@
 import sys
+from collections import defaultdict
 
 with open(sys.argv[1], "r") as f:
-    data = f.read().strip()
+    lines = f.readlines()
 
+path = []
+dirsizes = defaultdict(int)
+for line in lines:
+    tokens = line.strip().split(' ')
+    if tokens[0] == "$" and tokens[1] == "cd":
+        if tokens[2] == '/':
+            path = ['/']
+        elif tokens[2] == '..':
+            path.pop()
+        else:
+            path.append(tokens[2])
+    elif tokens[0] == "$" and tokens[1] == "ls":
+        pass
+    else:
+        try:
+            size = int(tokens[0])
+            for i in range(0, len(path)):
+                dirsizes['/'.join(path[:i+1])] += size
+        except ValueError:
+            pass
 
-print(f"part 1: {''}")
-print(f"part 2: {''}")
+part1 = sum(s for s in dirsizes.values() if s <= 100000)
+print(f"part 1: {part1}")
+
+disk_size = 70000000
+need = 30000000
+need_to_free = need - (disk_size - dirsizes['/'])
+
+# part2 = sorted(s for s in dirsizes.values() if s >= need_to_free)[0]
+part2 = min(s for s in dirsizes.values() if s >= need_to_free)
+print(f"part 2: {part2}")
